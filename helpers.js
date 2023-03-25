7e87c70334d047066b35e5ff53f455be6fb2c476
Guru Nanma P <42802604+18nanma@users.noreply.github.com>
2023-03-24 20:39:18 -0500

    bug: fixes, session to local storage

--- helpers.js
+++ helpers.js
@@ -12,7 +12,7 @@ var printTextarea = (function() {
     return function(text) {
         if (arguments.length > 1) text = Array.prototype.slice.call(arguments).join(' ');
         console.log(text);
-        sessionStorage.setItem('myText', text + "\n");
+        localStorage.setItem('myText', text + "\n");
         if (element) {
             element.value += text + "\n";
             element.scrollTop = element.scrollHeight; // focus on bottom
